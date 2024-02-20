97161882326ba053871edd8c698f072dffe3262d
SirCartierr <colintkiesling@gmail.com>
2024-02-20 07:37:31 -0600

    nvim boooost

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,22 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+
+    dashboard.section.header.val = {
+  [[                                     __                   ]],  
+  [[    ___       __     ___    __  __  /\_\     ___ ___      ]],        
+  [[  /' _ `\   /'__`\  / __`\ /\ \/\ \ \/\ \  /' __` __`\    ]],                                                        
+  [[  /\ \/\ \ /\  __/ /\ \L\ \\ \ \_/ | \ \ \ /\ \/\ \/\ \   ]],
+  [[  \ \_\ \_\\ \____\\ \____/ \ \___/   \ \_\\ \_\ \_\ \_\  ]],
+  [[   \/_/\/_/ \/____/ \/___/   \/__/     \/_/ \/_/\/_/\/_/  ]],
+    }
+
+    alpha.setup(dashboard.opts)
+  end,
+}
